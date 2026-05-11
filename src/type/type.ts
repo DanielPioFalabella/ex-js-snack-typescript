07b2b5255209c3e9dede70abc1319133a820d74c
Daniel Pio Falabella <danielpiofalabella001@gmail.com>
2026-05-11 10:49:49 +0200

    snack 3

--- src/type/type.ts
+++ src/type/type.ts
@@ -4,6 +4,18 @@ type Dipendente = {
     annoNascita: number,
     sesso: "m" | "f",
     anniDiServizio: number[],
-    readonly emailAziendale?: string,
+    readonly emailAziendale: string,
     contratto: "indeterminato" | "determinato" | "freelance"
+}
+
+type Developer = Dipendente & {
+    livelloEsperienza: "Junior" | "Mid" | "Senior",
+    linguaggi?: string[],
+    certificazioni: string[]
+}
+
+type ProjectManager = Dipendente & {
+    teamSize: number | null,
+    budgetGestito?: number,
+    stakeholderPrincipali: string[]
 }
